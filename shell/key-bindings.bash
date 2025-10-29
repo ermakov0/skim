91d3824fe3d6ef32459d7ec899b1c2684fbc6b1c
fox <ermakovav@altgtu.ru>
2025-10-29 11:34:09 +0700

    fix lintian errors

--- shell/key-bindings.bash
+++ shell/key-bindings.bash
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # - $SKIM_TMUX_OPTS
 # - $SKIM_CTRL_T_COMMAND
 # - $SKIM_CTRL_T_OPTS
--- shell/key-bindings.fish
+++ shell/key-bindings.fish
@@ -1,4 +1,4 @@
-#!/bin/fish
+#!/usr/bin/env fish
 # - $SKIM_TMUX_OPTS
 # - $SKIM_CTRL_T_COMMAND
 # - $SKIM_CTRL_T_OPTS
